dcd1df5964c90ce18e0b4b77e54e67e38a2f8d9d
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-12-30 21:43:09 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { MyPreset } from'../assests/themes/mytheme'
+
+
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset
+      }
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

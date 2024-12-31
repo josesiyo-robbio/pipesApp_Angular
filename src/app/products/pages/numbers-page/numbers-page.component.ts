2263ecb640cf9ae9f0eee2a126f0abafc2b531f4
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-12-31 00:44:35 -0600

    first commit

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   standalone: false,
-  
+
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
+  public totalSeals : number = 2323423423.4323423;
+  public percent : number = 0.5429;
 
 }
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,10 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   standalone: false,
-  
+
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
-export class UncommonPageComponent {
 
+
+export class UncommonPageComponent
+{
+  public namePerson: string = 'fernanada';
+  public gender: 'male' | 'female' = 'female';
+  public invitationMap = {
+    'male' : 'invitarlo',
+    'female' : 'invitarla'
+  }
+
+
+  changeClient()
+  {
+    this.namePerson = 'Steve';
+    this.gender = 'male';
+
+  }
 }

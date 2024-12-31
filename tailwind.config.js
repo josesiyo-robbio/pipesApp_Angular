dee42cb930573fd757df2ece534d358b2736048c
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-12-30 23:28:41 -0600

    cambios

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Incluye todos los archivos HTML y TS dentro de src
+  ],
+  theme: {
+    extend: {}, // Personaliza el tema aquí si es necesario
+  },
+  plugins: [],
+};
